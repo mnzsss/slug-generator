41c287288c188f57b299e9d2121d0fadd5c8a79e
Gabriel <gabrielsmenezes99@gmail.com>
2023-03-14 15:17:16 -0300

    feat: atualizar slugs no banco

--- index.ts
+++ index.ts
@@ -1,28 +1,30 @@
 /** @OnlyCurrentDoc */
 
+type Slug = {
+  sku: string;
+  current?: string;
+  recommended: string;
+};
+
 const enviroments = {
   Production: "",
   Staging: ".staging",
   Development: ".dev",
 };
 
-function run() {
+function getCampaignSuggestions() {
   try {
     const ss = SpreadsheetApp.getActiveSpreadsheet();
 
     // Config Sheet
-    var configSheet = ss.getSheetByName("Config");
+    const configSheet = ss.getSheetByName("Config");
 
     // Options
     const campaignId = configSheet.getRange("B2").getValue();
-    const configEnviroment = configSheet.getRange("B3").getValue();
+    const endpoint = getOptimusEndpoint();
 
-    const urlPrefix = "https://optimus";
-    const urlPrefixWithEnviroment = urlPrefix + enviroments[configEnviroment];
-    const endpoint = urlPrefixWithEnviroment + ".bbrands.com.br";
-
-    var response = UrlFetchApp.fetch(
-      `${endpoint}/api/campaign/slug-suggestions`,
+    const response = UrlFetchApp.fetch(
+      `${endpoint}/campaign/slug-suggestions`,
       {
         muteHttpExceptions: true,
         contentType: "application/json",
@@ -33,21 +35,67 @@ function run() {
       }
     ).getContentText();
 
-    var slugs = JSON.parse(response).map((slug) => [
-      slug.sku,
-      slug.current,
-      slug.recommended,
-      slug.current,
-    ]);
+    const slugs = (JSON.parse(response) as Slug[]).map((slug) => {
+      const recommended = slug.recommended.startsWith("/produtos")
+        ? slug.recommended
+        : `/produtos${slug.recommended}`;
 
-    var numRows = slugs.length;
-    var numCols = slugs[0].length;
+      return [slug.sku, slug.current, recommended, slug.current ?? recommended];
+    });
 
-    var suggestionsSheet = ss.getSheetByName("Sugestões");
+    const numRows = slugs.length;
+    const numCols = slugs[0].length;
+
+    const suggestionsSheet = ss.getSheetByName("Sugestões");
 
     suggestionsSheet.getRange(2, 1, numRows, numCols).setValues(slugs);
   } catch (error) {
     // deal with any errors
     Logger.log(error);
   }
+}
+
+function saveSlugs() {
+  try {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+
+    const suggestionsSheet = ss.getSheetByName("Sugestões");
+
+    const data = suggestionsSheet.getDataRange().getValues() as Array<string[]>;
+
+    const values = data.map((row) => ({
+      sku: row[0],
+      slug: row[2],
+    }));
+
+    const endpoint = getOptimusEndpoint();
+
+    UrlFetchApp.fetch(`${endpoint}/products-slug`, {
+      muteHttpExceptions: true,
+      contentType: "application/json",
+      method: "post",
+      payload: values,
+    });
+
+    ss.toast("Slugs Cadastrados!");
+  } catch (error) {
+    // deal with any errors
+    Logger.log(error);
+  }
+}
+
+function getOptimusEndpoint() {
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+
+  // Config Sheet
+  const configSheet = ss.getSheetByName("Config");
+
+  // Options
+  const configEnviroment = configSheet.getRange("B3").getValue();
+
+  const urlPrefix = "https://optimus";
+  const urlPrefixWithEnviroment = urlPrefix + enviroments[configEnviroment];
+  const endpoint = urlPrefixWithEnviroment + ".bbrands.com.br";
+
+  return `${endpoint}/api`;
 }
